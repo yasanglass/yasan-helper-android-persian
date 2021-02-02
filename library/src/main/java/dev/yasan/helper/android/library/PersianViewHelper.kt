5213ec95f2622af79b450be6426a2db7d3f76352
Yasan Ghafariyan <yasandev@gmail.com>
2021-02-02 19:16:11 +0330

    Add docs to PersianViewHelper.kt

--- library/src/main/java/dev/yasan/helper/android/library/PersianViewHelper.kt
+++ library/src/main/java/dev/yasan/helper/android/library/PersianViewHelper.kt
@@ -5,10 +5,24 @@ import android.view.View
 
 object PersianViewHelper {
 
+    /**
+     * Sets the activity's layout direction to right-to-left
+     */
     fun forceRTL(activity: Activity) { activity.window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL }
+
+    /**
+     * Sets the activity's layout direction to left-to-right
+     */
     fun forceLTR(activity: Activity) { activity.window.decorView.layoutDirection = View.LAYOUT_DIRECTION_LTR }
 
 }
 
+/**
+ * Sets the activity's layout direction to right-to-left
+ */
 fun Activity.forceRTL() = PersianViewHelper.forceRTL(this)
+
+/**
+ * Sets the activity's layout direction to left-to-right
+ */
 fun Activity.forceLTR() = PersianViewHelper.forceLTR(this)
